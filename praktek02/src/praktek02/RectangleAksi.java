071a544caf96f230a9ba8b4cf3df1c9a90fb8adc
nafisahcaca <imaryani397@gmail.com>
2018-04-04 23:38:57 +0800

    constructure dengan parameter

--- praktek02/src/praktek02/RectangleAksi.java
+++ praktek02/src/praktek02/RectangleAksi.java
@@ -8,5 +8,13 @@ public class RectangleAksi {
         
         rl.cetakInfo();
         System.out.println("luas rectangle ="+rl.hitungLuas());
+        rl.cetakLuas();
+        
+        Rectangle r2 = new Rectangle();
+        r2.cetakInfo();
+        
+        Rectangle r3 = new Rectangle();
+        r3.cetakInfo();
+        
     }
 }
